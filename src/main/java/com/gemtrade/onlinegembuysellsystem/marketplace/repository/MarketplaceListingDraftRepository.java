6b7717f3951f898e48571fa5bde76d679549c800
Chathurya <it24101093@my.sliit.lk>
2026-03-25 17:25:05 +0530

    Remove marketplace draft files from tracking before merge

--- src/main/java/com/gemtrade/onlinegembuysellsystem/marketplace/repository/MarketplaceListingDraftRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gemtrade.onlinegembuysellsystem.marketplace.repository;
-
-import com.gemtrade.onlinegembuysellsystem.marketplace.entity.MarketplaceListingDraft;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarketplaceListingDraftRepository extends JpaRepository<MarketplaceListingDraft, Long> {
-}
